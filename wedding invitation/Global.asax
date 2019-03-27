28a5d5c3e4d5a531b2d212942aac32618e7de644
RamkumarShanmugam <ramkumar.shanmugam@syncfusion.com>
2019-03-27 22:24:45 +0530

    Marriage invitation

---
+++ wedding invitation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wedding_invitation.MvcApplication" Language="C#" %>
---
+++ wedding invitation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wedding_invitation.Startup))]
+namespace wedding_invitation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
